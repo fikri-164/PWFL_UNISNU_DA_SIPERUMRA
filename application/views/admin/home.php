f7cde701e5647ccf1deb18d1d6d04a7f14a59618
Ahmad Hasan Umam Fikri <vickry97@gmail.com>
2019-10-16 13:09:42 +0700

    Pembuatan Template SB Admin Ke CI

---
+++ application/views/admin/home.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SI PERUMRA | Dashboard</title>
+</head>
+<body>
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            
+          </div>
+        </div>
+        <div class="col-lg">
+        <div class="card mb-4 py-3 border-bottom-warning">
+            <div class="card-body">
+            Selamat Datang Admin
+            </div>
+        </div>
+        </div>
+</body>
+</html>
---
+++ application/views/admin/profil.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>SI PERUMRA | Dashboard</title>
+</head>
+<body>
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Profil</h1>
+            
+          </div>
+          <div class="card shadow mb-4">
+                <!-- Card Header - Accordion -->
+                <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                  <h6 class="m-0 font-weight-bold text-primary">Collapsable Card Example</h6>
+                </a>
+                <!-- Card Content - Collapse -->
+                <div class="card mb-4">
+                  <div class="form-group">
+                    <label>Isi Profil</label>
+                    <textarea name="isi" class="form-control ckeditor" rows="5" >ggggggggggg</textarea>
+                  </div>
+                </div>
+              </div>
+        </div>
+</body>
+</html>
