961f1961bc54ea2c2d835ad7a81ce397710f6d9e
Cahya-Setyowati <56582744+Cahya-Setyowati@users.noreply.github.com>
2019-10-21 13:26:01 +0700

    menambahkan ckeditor.js
    menambahkan ckeditor.js pada assets untuk memudahkan edit tulisan pada teks area

--- application/views/admin/t_footer.php
+++ application/views/admin/t_footer.php
@@ -40,6 +40,7 @@
 
   <!-- Bootstrap core JavaScript-->
   <script src="<?php echo base_url ('assets/vendor/jquery/jquery.min.js')?>"></script>
+  <script src="<?php echo base_url ('assets/ckeditor/ckeditor.js')?>"></script>
   <script src="<?php echo base_url ('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
   <!-- Core plugin JavaScript-->
