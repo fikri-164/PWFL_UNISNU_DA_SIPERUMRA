48ca144ab365786205e5363e6c4f17c3b0607347
Cahya-Setyowati <56582744+Cahya-Setyowati@users.noreply.github.com>
2019-10-28 14:31:12 +0700

    membuat tabel database
    membuat tabel umkm dan produk pada database

---
+++ siperumra.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 28 Okt 2019 pada 08.27
+-- Versi Server: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `siperumra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `poduk`
+--
+
+CREATE TABLE `poduk` (
+  `id` int(50) NOT NULL,
+  `nama_umkm` varchar(50) NOT NULL,
+  `jenis_umkm` varchar(50) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `deskripsi_barang` text NOT NULL,
+  `harga_barang` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `umkm`
+--
+
+CREATE TABLE `umkm` (
+  `id` int(11) NOT NULL,
+  `nama_umkm` varchar(50) NOT NULL,
+  `jenis_umkm` varchar(50) NOT NULL,
+  `profil` varchar(50) NOT NULL,
+  `cp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `nama_pemilik` varchar(50) NOT NULL,
+  `nama_umkm` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `nama_pemilik`, `nama_umkm`, `password`, `level`) VALUES
+(1, 'fikri', 'suka', '21232f297a57a5a743894a0e4a801fc3', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `poduk`
+--
+ALTER TABLE `poduk`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `umkm`
+--
+ALTER TABLE `umkm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `poduk`
+--
+ALTER TABLE `poduk`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `umkm`
+--
+ALTER TABLE `umkm`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
